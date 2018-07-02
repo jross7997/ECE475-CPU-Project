91864df5898451a859ce37c3c2ce339c21af8ed9
Justin Ross <40772108+jross7997@users.noreply.github.com>
2018-07-02 19:48:42 -0400

    Add files via upload

---
+++ Main/DataPath/MainMemory/MainMemory.ip_user_files/ip/MainMemory/MainMemory_stub.vhdl
@@ -0,0 +1,35 @@
+-- Copyright 1986-2015 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2015.4 (win64) Build 1412921 Wed Nov 18 09:43:45 MST 2015
+-- Date        : Tue Apr 24 16:55:12 2018
+-- Host        : SHIN436-PC-07 running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               c:/Users/jross5/Desktop/DataPath/MainMemory/MainMemory.srcs/sources_1/ip/MainMemory/MainMemory_stub.vhdl
+-- Design      : MainMemory
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a100tcsg324-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity MainMemory is
+  Port ( 
+    clka : in STD_LOGIC;
+    ena : in STD_LOGIC;
+    wea : in STD_LOGIC_VECTOR ( 0 to 0 );
+    addra : in STD_LOGIC_VECTOR ( 4 downto 0 );
+    dina : in STD_LOGIC_VECTOR ( 63 downto 0 );
+    douta : out STD_LOGIC_VECTOR ( 63 downto 0 )
+  );
+
+end MainMemory;
+
+architecture stub of MainMemory is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clka,ena,wea[0:0],addra[4:0],dina[63:0],douta[63:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "blk_mem_gen_v8_3_1,Vivado 2015.4";
+begin
+end;
---
+++ Main/DataPath/MainMemory/MainMemory.ip_user_files/sim_scripts/MainMemory/xsim/filelist.f
@@ -0,0 +1,2 @@
+../../../ipstatic/blk_mem_gen_v8_3_1/simulation/blk_mem_gen_v8_3.vhd
+../../../../MainMemory.srcs/sources_1/ip/MainMemory/sim/MainMemory.vhd
